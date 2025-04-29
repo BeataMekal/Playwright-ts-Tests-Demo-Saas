2ebf0e6ad21de3a98ee1ef907078c73890d59372
Beata Lapek <beatakuzlecka@o2.pl>
2025-04-29 12:37:23 +0200

    chore: update env variables

--- config/config-variables.ts
+++ config/config-variables.ts
@@ -1,2 +1,2 @@
-export const username  = process.env.DEMO_SAAS_USERNAME! ?? "betikotek@gmail.com";
-export const password = process.env.DEMO_SAAS_PASSWORD! ?? "Test1234";
+export const username  = process.env.DEMO_SAAS_USERNAME!;
+export const password = process.env.DEMO_SAAS_PASSWORD!;
