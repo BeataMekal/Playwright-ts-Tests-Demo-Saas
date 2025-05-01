c233efdde88aedc1868c133e665336fa6cde5a7a
Beata Lapek <beatakuzlecka@o2.pl>
2025-05-01 14:38:46 +0200

    chore: update title of tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
-
 import {username, password} from '../config/config-variables'
 import { LoginPage } from '../src/pages/login.page';
+
+let loginPage: LoginPage;
+
 test.describe('Verify Log in to the system', () => {
   test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
+    loginPage = new LoginPage(page);
     await page.goto('');
 
     await expect(page).toHaveTitle(/Demo SaaS/);
@@ -13,9 +15,7 @@ test.describe('Verify Log in to the system', () => {
     await expect(loginPage.loginHeading).toBeVisible();
   });
 
-  test('Successful Log in', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
+  test('Log in with correct credentials', async ({ page }) => {
     await loginPage.emailInput.fill(username);
     await loginPage.passwordInput.fill(password);
 
@@ -24,8 +24,7 @@ test.describe('Verify Log in to the system', () => {
     await expect(loginPage.pageHeading).toBeVisible();
   });
 
-  test('Unsuccessful log in to system - invalid email', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+  test('Reject login with incorrect email', async ({ page }) => {
     const invalidEmail = 'eee@ttt.pl';
 
     await loginPage.emailInput.fill(invalidEmail);
@@ -35,8 +34,7 @@ test.describe('Verify Log in to the system', () => {
 
     await expect(loginPage.loginErrorMessage).toBeVisible();
   });
-  test('Unsuccessful log in to system - invalid email without @', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+  test('Reject login with incorrect email without @', async ({ page }) => {
     const invalidEmail = 'test.gmail.com';
    
     await loginPage.emailInput.fill(invalidEmail);
@@ -47,8 +45,7 @@ test.describe('Verify Log in to the system', () => {
     await expect(loginPage.invalidEmailErrorMessage).toBeVisible();
   });
   
-  test('Unsuccessful log in to the system - invalid password', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+  test('Reject login with incorrect password', async ({ page }) => {
     const invalidPassword = 'Test';
     
     await loginPage.emailInput.fill(username);
