51895bfc50eedc9c9d7bb5b5f4460d868b33a927
Beata Lapek <beatakuzlecka@o2.pl>
2025-05-06 13:15:42 +0200

    chore: add basepage

---
+++ src/pages/base.page.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  url = '';
+  constructor(protected page: Page) {}
+
+  async goto(parameters = ''): Promise<void> {
+    await this.page.goto(`${this.url}${parameters}`);
+  }
+
+  async waitForPageToLoadUrl(): Promise<void> {
+    await this.page.waitForURL(this.url);
+  }
+}
