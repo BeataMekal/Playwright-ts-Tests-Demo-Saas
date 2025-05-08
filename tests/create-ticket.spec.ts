93debbcb015d35ee9632a9e7c1fb62a0eabdf95b
Beata Lapek <beatakuzlecka@o2.pl>
2025-05-08 11:54:57 +0200

    chore: add ticket with one character test

--- tests/create-ticket.spec.ts
+++ tests/create-ticket.spec.ts
@@ -47,4 +47,41 @@ test.describe('Verify Log in to the system', () => {
     await expect(ticketsPage.createdAtTextFirst).toHaveText(createdNowText);
 
     });
+  test('Should create ticket with one character in all fields', async ({ page }) => {
+    const ticketsPage = new TicketsPage(page);
+    const ticketsNewPage = new TicketsNewPage(page);
+    const ticketPage = new TicketPage(page);
+    const createdNowText = 'now';
+    const statusBadgeText = 'New';
+    const nameText = 'A';
+    const titleText = 't';
+    const descriptionText = 'd';
+
+    //wait for tickets in table
+    await expect(ticketsPage.getReportedByValueLocator(page, username)).toBeVisible();
+
+    await ticketsPage.newButton.click();
+    await expect(ticketsNewPage.submitNewTicketHeading).toBeVisible();
+    await expect(ticketsNewPage.reportedByInput).toHaveValue(username);
+    await ticketsNewPage.yourNameInput.fill(nameText);
+    await ticketsNewPage.titleInput.fill(titleText);
+    await ticketsNewPage.descriptionInput.fill(descriptionText);
+    await ticketsNewPage.submitButton.click();
+
+    await expect(ticketPage.createdTicketAlert).toBeVisible();
+    await expect(ticketPage.titleText).toHaveText(titleText);
+    await expect(ticketPage.reportedByText).toHaveText(`${nameText} (${username})`);
+    await expect(ticketPage.createdText).toHaveText(createdNowText);
+    await expect(ticketPage.descriptionText).toHaveText(descriptionText);
+    await expect(ticketPage.statusBadge).toHaveText(statusBadgeText);
+
+    await ticketsPage.goto();
+
+    await expect(ticketsPage.titleTextFirst).toHaveText(titleText);
+    await expect(ticketsPage.getReportedByValueLocator(page, username)).toBeVisible();
+    await expect(ticketsPage.statusTextFirst).toHaveText(statusBadgeText);
+    await expect(ticketsPage.createdAtTextFirst).toHaveText(createdNowText);
+
+    });
+    
 });
